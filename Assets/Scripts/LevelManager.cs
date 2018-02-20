c30769ce5c5e9f86f1e107752aefbeff10b64ba5
Donutcat <31439092+donutcatscience@users.noreply.github.com>
2018-02-19 20:00:06 -0700

    One Hour Update
    Added:
    *Castle,Town,Nature packs
    *FPS controller and character
    *Started layout of Outer and Inner Walls

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    //load game scene
+    public void LoadLevel (string name)
+    {
+        Debug.Log("Level load requested for " +name);
+        Application.LoadLevel(name);
+    }
+
+    //handle quitting game
+    public void QuitRequest()
+    {
+        Debug.Log("I want to quit!");
+        Application.Quit();
+    }
+}
