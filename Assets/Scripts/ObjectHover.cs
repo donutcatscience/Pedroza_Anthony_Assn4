db023497a9bd3ecdb78041a52f76ca45abd02fda
Donutcat <31439092+donutcatscience@users.noreply.github.com>
2018-02-20 22:05:26 -0700

    Four Hour Update
    Added:
    Gun, King, Crosshair
    Camera and Character controls

---
+++ Assets/Scripts/ObjectHover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ObjectHover : MonoBehaviour {
+    public GameObject Object;
+    private Vector3 offset;
+
+
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - Object.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+        private void LateUpdate()
+    {
+        transform.position = Object.transform.position + offset;
+    }
+}
